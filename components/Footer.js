9c08fac9d9565444b4bb21a71bc2a075572c7c4c
ankitkumar <keshavankit89@gmail.com>
2025-05-18 15:24:26 +0530

    edit footer

--- components/Footer.js
+++ components/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-500 dark:text-gray-400 text-sm">
-            © {currentYear} My Portfolio. All rights reserved.
+            © {currentYear} ankitbishwas All rights reserved.
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             {socialLinks.map((link) => (
